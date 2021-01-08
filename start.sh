0e03e1f0b8720ef0306d0bcc71c6fbf0a9a29312
Jonathan Arndt <jarndt@salesforce.com>
2021-01-07 23:49:05 -0700

    cleaning up

--- start.sh
+++
@@ -1,4 +0,0 @@
-name=damage_calc
-docker ps -a | grep ${name} > /dev/null 2>&1 && { docker stop ${name}; docker rm ${name}; }
-docker build -t jarndt/damage-calc .
-docker run -p 1000:80 --name ${name} -d jarndt/damage-calc
